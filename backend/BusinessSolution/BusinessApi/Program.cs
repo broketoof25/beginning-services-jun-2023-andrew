d3c47041e0dadd02b154f29a67fdac4fed149a6b
Andrew Lall <andrew.s.lall@gmail.com>
2023-06-13 09:45:55 -0400

    Initial commit

---
+++ backend/BusinessSolution/BusinessApi/Program.cs
@@ -0,0 +1,37 @@
+
+
+
+using Microsoft.AspNetCore.Mvc;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+// Transient, Scoped, and Singleton - tomorrow morning we will learn how to choose these.
+builder.Services.AddScoped<BusinessClock>();
+builder.Services.AddSingleton<ISystemTime, SystemTime>();
+
+// everything above here is configuring the "guts" of our API
+var app = builder.Build();
+// everything AFTER here is setting up the Request/Response pipeline.
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.MapGet("/clock", ([FromServices] BusinessClock businessClock) =>
+{
+  
+    var response = businessClock.GetClockResponse();
+    return Results.Ok(response);
+});
+// Start the Web Server (Kestrel)
+
+app.Run();
+
+
